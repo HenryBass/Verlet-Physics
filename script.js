beb3041e96bfb0b26bf417d6ef888de84f4582ec
henry <63601449+HenryBass@users.noreply.github.com>
2023-08-07 14:20:13 -0400

    movable boxes

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 width = 1024
 height = 512
 
-dt = 0.001
-
 t = 0
 
 const canvas = document.getElementById('canvas');
@@ -15,6 +13,8 @@ let key = ""
 let g = 0.1
 let maxStress = 100
 let linkStrength = 1
+
+let barriers = []
 
 lastTime = 0
 
@@ -30,6 +30,63 @@ function findatloc(x, y) {
         }
     }
     return "Fail"
+}
+
+class FixedRect {
+    constructor(x, y, w, h, draggable = false) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.draggable = draggable
+        this.beingDragged = false
+        this.startDragMouseX = 0
+        this.startDragMouseY = 0
+    }
+    overlaps(x, y, r) {
+        let xoff = 0
+        let yoff = 0
+
+        let leftXoff = Math.max((x + r) - this.x, 0)
+        let rightXoff = Math.min((x - r) - (this.x + this.w), 0)
+        let topYoff = Math.max((y + r) - this.y, 0)
+        let bottomYoff = Math.min((y - r) - (this.y + this.h), 0)
+        console.log(leftXoff, rightXoff, topYoff, bottomYoff)
+        if (Math.abs(leftXoff) < Math.abs(rightXoff)) {
+            xoff = leftXoff
+        } else {
+            xoff = rightXoff
+        }
+        if (Math.abs(topYoff) < Math.abs(bottomYoff)) {
+            yoff = topYoff
+        } else {
+            yoff = bottomYoff
+        }
+
+        if (Math.abs(xoff) < Math.abs(yoff)) {
+            yoff = 0
+        } else {
+            xoff = 0
+        }
+    
+        return [xoff, yoff]
+    }
+    update() {
+        if (this.draggable) {
+            let overlap = this.overlaps(mouseX, mouseY, 0)
+            if ((overlap[0] !== 0 || overlap[1] !== 0) && mousedown) {
+                if (this.beingDragged == false) {
+                    this.startDragMouseX = mouseX - this.x
+                    this.startDragMouseY = mouseY - this.y
+                }
+                this.beingDragged = true
+                this.x = mouseX - this.startDragMouseX
+                this.y = mouseY - this.startDragMouseY
+            } else {
+                this.beingDragged = false
+            }
+        }
+    }
 }
 
 class Entity {
@@ -93,55 +150,62 @@ class Entity {
                 }
             }
         }
+        for (let i = 0; i < barriers.length; i++) {
+             let [xoff, yoff] = barriers[i].overlaps(this.x, this.y, this.radius)
+                this.x -= xoff
+                this.y -= yoff
+            
+        }
     }
 
     update() {
-
-        if (this.fixed == false) {
 
             for (let i = 0; i < this.links.length; i++) {
                 if (this.links[i] !== undefined) {
                     let other = this.links[i]
                     let stress = dist(this.x, this.y, other.x, other.y)
                     if (stress < maxStress) {
-                        if (!other.fixed) {
-                            other.x -= ((other.x - this.x) * stress) * linkStrength / 1000
-                            other.y -= ((other.y - this.y) * stress) * linkStrength / 1000
-                            this.x += ((other.x - this.x) * stress)  * linkStrength / 1000
-                            this.y += ((other.y - this.y) * stress)  * linkStrength / 1000
-                        } else {
-                            this.x += ((other.x - this.x) * stress) * linkStrength / 500
-                            this.y += ((other.y - this.y) * stress) * linkStrength / 500
+                        if (!this.fixed) {
+                            if (!other.fixed) {
+                                other.x -= ((other.x - this.x) * stress) * linkStrength / 1000
+                                other.y -= ((other.y - this.y) * stress) * linkStrength / 1000
+                                this.x += ((other.x - this.x) * stress)  * linkStrength / 1000
+                                this.y += ((other.y - this.y) * stress)  * linkStrength / 1000
+                            } else {
+                                this.x += ((other.x - this.x) * stress) * linkStrength / 500
+                                this.y += ((other.y - this.y) * stress) * linkStrength / 500
+                            }
                         }
                     } else {
-                        this.links.splice(i, 1)
-                        other.links.splice(this, 1)
+                        this.links.splice(this.links.indexOf(other), 1)
+                        other.links.splice(other.links.indexOf(this), 1)
                     }
                     }
     
             }
         
-        this.accelerate(0, g)
+        if (this.fixed == false) {
+            this.accelerate(0, g)
 
- 
-        t += 1
-        for (let i = 0; i < 8; i++) {
-            this.substep();
-        }
+    
+            t += 1
+            for (let i = 0; i < 8; i++) {
+                this.substep();
+            }
 
-        let velx = (this.x - this.lx) * 0.99;
-        let vely = (this.y - this.ly) * 0.99;
+            let velx = (this.x - this.lx) * 0.95;
+            let vely = (this.y - this.ly) * 0.95;
 
-        this.fill[0] = dist(this.x, this.y, this.lx, this.ly) * 20
+            this.fill[0] = dist(this.x, this.y, this.lx, this.ly) * 20
 
-        this.lx = this.x;
-        this.ly = this.y;
+            this.lx = this.x;
+            this.ly = this.y;
 
-        this.x += velx + this.accx;
-        this.y += vely + this.accy;
+            this.x += Math.min(velx + this.accx, 30);
+            this.y += Math.min(vely + this.accy, 30);
 
-        this.accx = 0;
-        this.accy = 0;
+            this.accx = 0;
+            this.accy = 0;
         } else {
             this.fill = [128, 128, 128]
         }
@@ -170,10 +234,20 @@ canvas.addEventListener("mousedown", function(e) {
     if (key == "Shift") {
         let loc = findatloc(mouseX, mouseY)
         if (loc != "Fail") {
+            loc.lx = loc.x
+            loc.ly = loc.y
             loc.fixed = !loc.fixed
         }
     } else if (key == "Control") {
-        entities.push(new Entity(mouseX, mouseY, 10));
+        let canPlace = true
+        for (let i = 0; i < barriers.length; i++) {
+            let overlap = barriers[i].overlaps(mouseX, mouseY, 0)
+            if (overlap[0] != 0 || overlap[1] != 0) {
+                canPlace = false
+            }
+        }
+        console.log(canPlace)
+        if (canPlace) entities.push(new Entity(mouseX + Math.random(), mouseY + Math.random(), 10));
 }    else {
         mousedown = true
         let loc = findatloc(mouseX, mouseY)
@@ -202,12 +276,10 @@ addEventListener("keydown", function(e) {
     } else if (entities.length > 1 && key == "d") {
         oentry = findatloc(mouseX, mouseY)
         if (oentry !== "Fail") {
-            entities.splice(entities.indexOf(oentry), 1)
-            for (let i = 0; i < entities.length; i++) {
-                if (entities[i].links.indexOf(oentry) != -1) {
-                    entities[i].links.splice(oentry, 1)
-                }
+            for (let i = 0; i < oentry.links.length; i++) {
+                oentry.links[i].links.splice(oentry.links[i].links.indexOf(oentry), 1)
             }
+            entities.splice(entities.indexOf(oentry), 1)
         }
     }
 })
@@ -245,7 +317,7 @@ function render() {
         if (e.links.length > 0) {
             for(let i = 0; i < e.links.length; i++) {
                 stressRatio = maxStress/dist(e.x, e.y, e.links[i].x, e.links[i].y)
-                ctx.lineWidth = ((stressRatio) + 3);
+                ctx.lineWidth = Math.min((stressRatio) + 3, 4);
                 ctx.strokeStyle = `rgb(${255 - ((stressRatio * 0.25) * 150)}, 100, 100)`;
                 ctx.beginPath();
                 ctx.moveTo(e.x, e.y);
@@ -258,6 +330,11 @@ function render() {
     }
     ctx.strokeStyle = "rgb(0, 0, 0)"
     ctx.lineWidth = 0;
+    for (let i = 0; i < barriers.length; i++) {
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        ctx.fillRect(barriers[i].x, barriers[i].y, barriers[i].w, barriers[i].h);
+        barriers[i].update()
+    }
 }
 
 function frameRate() {
@@ -270,6 +347,9 @@ function frameRate() {
 function setup() {
     entities = [];
     entities.push(selected)
+    barriers.push(new FixedRect(0, height-300, 300, 300))
+    barriers.push(new FixedRect(width-300, height-300, 300, 300))
+    barriers.push(new FixedRect(width/2-150, height-300, 300, 300, true))
 }
 
 function draw() {
